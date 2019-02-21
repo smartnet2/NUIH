5a428ac27b295ee05ee9c06d31bc48af1e0a9c8f
Krishna <krishinbox2@gmail.com>
2019-02-21 18:44:56 +0530

    Revert "SSL Changes"
    This reverts commit 71b85190fffe082df43f48aadf82f1b8943d78c2.

--- src/app/client/src/app/modules/public/module/nuih/components/index.ts
+++ src/app/client/src/app/modules/public/module/nuih/components/index.ts
@@ -5,4 +5,3 @@ export * from './data-exchange/data-exchange.component';
 export * from './smart-gov/smart-gov.component';
 export * from './about-us/about-us.component';
 export * from './coming-soon/coming-soon.component';
-export * from './ssl-verify/ssl-verify.component';
