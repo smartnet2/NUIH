84ae9816db2c65de04ad47bf508a7f6123505d6e
Ram Kumar <ramkumar.s@posterotech.com>
2019-02-21 14:42:02 +0530

    Saving the local files

---
+++ src/app/client/src/app/modules/public/module/nuih/nuih.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TelemetryModule } from '@sunbird/telemetry';
+import { CoreModule } from '@sunbird/core';
+import { SharedModule } from '@sunbird/shared';
+import { NgInviewModule } from 'angular-inport';
+import { NuihComponent } from './components/nuih/nuih.component';
+import { LearnComponent } from './components/learn/learn.component';
+import { InnovateComponent } from './components/innovate/innovate.component';
+import { DataExchangeComponent } from './components/data-exchange/data-exchange.component';
+import { SmartGovComponent } from './components/smart-gov/smart-gov.component';
+import { AboutusComponent } from './components/about-us/about-us.component';
+import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TelemetryModule,
+    CoreModule,
+    SharedModule,
+    NgInviewModule
+  ],
+  declarations: [NuihComponent, LearnComponent, InnovateComponent, DataExchangeComponent, SmartGovComponent, AboutusComponent, ComingSoonComponent]
+})
+export class NuihModule { }
