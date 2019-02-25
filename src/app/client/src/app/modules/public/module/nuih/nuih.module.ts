bdba769ac341dfe416006372d569a3692f983866
Krishna <krishinbox2@gmail.com>
2019-02-25 23:36:42 +0530

    Email Id submission included

--- src/app/client/src/app/modules/public/module/nuih/nuih.module.ts
+++ src/app/client/src/app/modules/public/module/nuih/nuih.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { TelemetryModule } from '@sunbird/telemetry';
 import { CoreModule } from '@sunbird/core';
@@ -11,16 +13,21 @@ import { DataExchangeComponent } from './components/data-exchange/data-exchange.
 import { SmartGovComponent } from './components/smart-gov/smart-gov.component';
 import { AboutusComponent } from './components/about-us/about-us.component';
 import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
-
-
+import { EmailInputComponent } from './common/components/email-input/email-input.component';
+import { WriteEmailService } from './common/services/email-input/email-input.service';
+import { EmailService } from './common/services/email/email.service';
+import { DataService } from '../../../core/services/data/data.service';
 @NgModule({
   imports: [
+    BrowserModule,
+    FormsModule,
     CommonModule,
     TelemetryModule,
     CoreModule,
     SharedModule,
     NgInviewModule
   ],
-  declarations: [NuihComponent, LearnComponent, InnovateComponent, DataExchangeComponent, SmartGovComponent, AboutusComponent, ComingSoonComponent]
+  declarations: [NuihComponent, LearnComponent, InnovateComponent, DataExchangeComponent, SmartGovComponent, AboutusComponent, ComingSoonComponent, EmailInputComponent],
+  providers: [DataService, EmailService, WriteEmailService]
 })
 export class NuihModule { }
---
+++ src/app/helpers/custom/customServiceHelper.js
@@ -0,0 +1,74 @@
+const credentials = {
+    AZURE_ACCOUNT_NAME: 'nuihsunbird',
+    AZURE_ACCOUNT_KEY: 'PiAc4nsoLrcyfxqH2E4PEiNeEZ2Ji42LiyGKe698a46DAaOLEqS3CbS9gMzdYBQBIEjO+f0uIj9szZBvEosDVQ==',
+    AZURE_STORAGE_URL: 'https://nuihsunbird.blob.core.windows.net/',
+    EMAIL_STORE_CONTAINER_NAME: 'emails'
+}
+
+var fs = require('fs');
+var azure = require('azure-storage');
+var blobService = azure.createBlobService(credentials.AZURE_ACCOUNT_NAME, credentials.AZURE_ACCOUNT_KEY);
+
+function verifyAndStoreEmail(req, res) {
+
+    console.log(req.body)
+    var email = req.body.emailid;
+    console.log('email is : ' + email)
+    verifyOrStoreEmail(email, function (err) {
+        if (err) {
+            console.log('error in uploading email blob to azure ! ' + JSON.stringify(err));
+            //res.json({result: 'error in uploading email to azure ! '+JSON.stringify(err),success:false})
+            res.status(500)
+            res.send({
+                'id': null,
+                'ver': null,
+                'ts': null,
+                'params': {
+                    'resmsgid': null,
+                    'msgid': null,
+                    'status': 'failed',
+                    'err': '',
+                    'errmsg': null
+                },
+                'responseCode': 500,
+                'result': {}
+            })
+            res.end()
+        } else {
+            console.log('uploaded email blob successfully to azure');
+            //res.json({success:true, result: 'uploaded email blob successfully to azure'})
+            res.status(200)
+            res.send({
+                'id': null,
+                'ver': null,
+                'ts': null,
+                'params': {
+                    'resmsgid': null,
+                    'msgid': null,
+                    'status': 'success',
+                    'err': '',
+                    'errmsg': null
+                },
+                'responseCode': 'OK',
+                'result': {}
+            })
+            res.end()
+        }
+    })
+}
+
+function verifyOrStoreEmail(email, callback) {
+
+    blobService.createBlockBlobFromText(credentials.EMAIL_STORE_CONTAINER_NAME, email, email, function (error, result, response) {
+        if (!error) {
+            // file uploaded
+            console.log('blob result ' + JSON.stringify(response));
+            return callback(null);
+        } else {
+            console.log(error)
+            return callback(error)
+        }
+    });
+}
+
+module.exports.verifyAndStoreEmail = verifyAndStoreEmail;
